246dbf553ffc43e8c0ac80bf91dc6c3782c3aee6
kickinbahk <josiahmory@gmail.com>
2016-02-19 19:39:37 -0800

    Refactor middleware to own file

---
+++ middleware.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+
+
+
+var middleware = {
+    requireAuthentication: function(req, res, next) {
+        console.log(`Private route hit`);
+        next();
+    },
+    logger: function (req, res, next) {
+        var date = new Date().toString();
+        console.log(`Request: ${date} ${req.method} ${req.originalUrl}`);
+        next();
+    }
+};
+
+module.exports = middleware;
--- server.js
+++ server.js
@@ -1,18 +1,7 @@
 var express = require('express');
 var app = express();
-var port = 3000;
-
-var middleware = {
-    requireAuthentication: function(req, res, next) {
-        console.log(`Private route hit`);
-        next();
-    },
-    logger: function (req, res, next) {
-        var date = new Date().toString();
-        console.log(`Request: ${date} ${req.method} ${req.originalUrl}`);
-        next();
-    }
-};
+var middleware = require("./middleware.js");
+var PORT = 3000;
 
 app.use(middleware.logger);
     
@@ -23,6 +12,6 @@ app.get('/about', middleware.requireAuthentication, function(req, res) {
 });
 
 app.use(express.static(`${__dirname}/public`));
-app.listen(port, function() {
-    console.log(`Express server started on port ${port}`);
+app.listen(PORT, function() {
+    console.log(`Express server started on port ${PORT}`);
 });
