a6abcd82e240ddbb4686d8bc928ef097c55c8f08
kickinbahk <kickinbahk@gmail.com>
2016-02-19 18:42:06 -0800

    remove root get, add port var

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 
 var express = require('express');
 var app = express();
-
-app.get('/', function(req, res) {
-    res.send(`Hello, Express!`);
-});
+var port = 3000;
 
 app.get('/about', function(req, res) {
     res.send(`About Us`);
 });
 
 app.use(express.static(`${__dirname}/public`));
-app.listen(3000);
+app.listen(port, function() {
+    console.log(`Express server started on port ${port}`);
+});
