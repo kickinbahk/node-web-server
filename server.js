5cff7d9475a93cf3245f421f233fcb45584fb382
kickinbahk <kickinbahk@gmail.com>
2016-02-19 18:57:45 -0800

    add logger to middleware

--- server.js
+++ server.js
@@ -6,9 +6,15 @@ var middleware = {
     requireAuthentication: function(req, res, next) {
         console.log(`Private route hit`);
         next();
+    },
+    logger: function (req, res, next) {
+        console.log(`${req.method} ${req.originalUrl}`);
+        next();
     }
 };
 
+app.use(middleware.logger);
+    
 // app.use(middleware.requireAuthentication);
 
 app.get('/about', middleware.requireAuthentication, function(req, res) {
